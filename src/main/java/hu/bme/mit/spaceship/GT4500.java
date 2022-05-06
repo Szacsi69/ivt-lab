d63375109d14994d4762e85637829bbe9b280e0b
Szacsi69 <szabo01gergo@gmail.com>
2022-05-06 13:45:27 +0200

    6. Gyakorlat

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,9 +10,9 @@ public class GT4500 implements SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new TorpedoStore(10);
-    this.secondaryTorpedoStore = new TorpedoStore(10);
+  public GT4500(TorpedoStore p, TorpedoStore s) {
+    this.primaryTorpedoStore = p;
+    this.secondaryTorpedoStore = s;
   }
 
   public boolean fireLaser(FiringMode firingMode) {
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -3,6 +3,7 @@ package hu.bme.mit.spaceship;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
@@ -10,31 +11,59 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
+
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primary = Mockito.mock(TorpedoStore.class);
+    secondary = Mockito.mock(TorpedoStore.class);
+
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    Mockito.when(primary.isEmpty()).thenReturn(false);
+    Mockito.when(primary.fire(1)).thenReturn(true);
+
+    Mockito.when(secondary.isEmpty()).thenReturn(false);
+    Mockito.when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(primary, times(1)).isEmpty();
+    verify(primary, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    Mockito.when(primary.isEmpty()).thenReturn(false);
+    Mockito.when(primary.getTorpedoCount()).thenReturn(10);
+    Mockito.when(primary.fire(primary.getTorpedoCount())).thenReturn(true);
+
+    Mockito.when(secondary.isEmpty()).thenReturn(false);
+    Mockito.when(secondary.getTorpedoCount()).thenReturn(10);
+    Mockito.when(secondary.fire(secondary.getTorpedoCount())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primary, times(1)).isEmpty();
+    verify(primary, times(2)).getTorpedoCount();
+    verify(primary, times(2)).fire(primary.getTorpedoCount());
+
+    verify(secondary, times(1)).isEmpty();
+    verify(secondary, times(2)).getTorpedoCount();
+    verify(secondary, times(2)).fire(secondary.getTorpedoCount());
   }
 
 }
